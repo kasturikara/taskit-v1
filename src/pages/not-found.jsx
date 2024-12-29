547bf16f0266004deb22b769796393d2152c845f
kasturikara <littlecommander46@gmail.com>
2024-12-29 17:21:49 +0700

    Enhance Not Found page with improved layout and messaging

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,8 +1,25 @@
 const NotFoundPages = () => {
   return (
-    <div>
-      <h1>NotFoundPages</h1>
-      <button onClick={() => window.history.back()}>kembali</button>
+    <div className="flex items-center h-screen p-16 text-gray-800 bg-gray-50">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-gray-400 text-9xl">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl">
+            Halaman yang Anda cari tidak ditemukan.
+          </p>
+          <p className="mt-4 mb-8 text-lg text-gray-600">
+            Silahkan periksa kembali URL atau kembali ke halaman sebelumnya.
+          </p>
+          <button
+            onClick={() => window.history.back()}
+            className="px-8 py-3 font-semibold text-white rounded bg-sky-600"
+          >
+            Kembali
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
